58a26da31e5af6aaca71183de2f88a5079a41e34
Phil Nash <github@philnash.me>
2012-08-07 08:18:33 +0100

    Refactored impls

---
+++ include/internal/catch_interfaces_generators.h
@@ -0,0 +1,14 @@
+//
+//  catch_interfaces_generators.h
+//  CatchSelfTest
+//
+//  Created by Phil Nash on 07/08/2012.
+//
+//
+
+#ifndef CatchSelfTest_catch_interfaces_generators_h
+#define CatchSelfTest_catch_interfaces_generators_h
+
+
+
+#endif
