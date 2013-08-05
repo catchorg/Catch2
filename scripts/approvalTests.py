f3119b0fcf6108771350f959f418ec92beb87b19
Kosta <github@ks-baumann.de>
2013-08-05 13:22:23 +0200

    more debugging

--- scripts/approvalTests.py
+++ scripts/approvalTests.py
@@ -15,7 +15,7 @@ baselinesPath = os.path.join( catchPath, 'projects/SelfTest/Baselines/approvedRe
 rawResultsPath = os.path.join( catchPath, 'projects/SelfTest/Baselines/_rawResults.tmp' )
 filteredResultsPath = os.path.join( catchPath, 'projects/SelfTest/Baselines/unapprovedResults.txt' )
 
-print "len(sys.argv) = " + len(sys.argv)
+print "len(sys.argv) = " + str(len(sys.argv))
 print "sys.argv[0] = " + sys.argv[0]
 print "sys.argv[1] = " + sys.argv[1]
 
