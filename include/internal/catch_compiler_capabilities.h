16a7db036f04559a4d369b0b8a2b839c40eafbb5
Phil Nash <github@philnash.me>
2014-09-18 18:23:47 +0100

    enable nullptr for VS2010

--- include/internal/catch_compiler_capabilities.h
+++ include/internal/catch_compiler_capabilities.h
@@ -80,6 +80,10 @@
 // Visual C++
 #ifdef _MSC_VER
 
+#if (_MSC_VER >= 1600)
+#define CATCH_CONFIG_CPP11_NULLPTR
+#endif
+
 #if (_MSC_VER >= 1310 ) // (VC++ 7.0+)
 //#define CATCH_CONFIG_SFINAE // Not confirmed
 #endif
