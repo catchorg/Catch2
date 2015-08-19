f31903ca3fbfcf35ecbad7e1e65185271bf87f8f
James Trey Nickelsen <tarnasa7@gmail.com>
2015-08-19 14:52:24 -0500

    [On behalf of Lexmark]:  Initialize scalar fields in catch_test_spec_parser.hpp

--- include/internal/catch_test_spec_parser.hpp
+++ include/internal/catch_test_spec_parser.hpp
@@ -29,7 +29,12 @@ namespace Catch {
         ITagAliasRegistry const* m_tagAliases;
 
     public:
-        TestSpecParser( ITagAliasRegistry const& tagAliases ) : m_tagAliases( &tagAliases ) {}
+        TestSpecParser( ITagAliasRegistry const& tagAliases )
+        :   m_tagAliases( &tagAliases ),
+            m_mode( None ),
+            m_exclusion( false ),
+            m_start( std::string::npos ),
+            m_pos( 0 ) {}
 
         TestSpecParser& parse( std::string const& arg ) {
             m_mode = None;
