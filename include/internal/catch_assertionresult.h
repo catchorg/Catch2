4eb19add11e57c76313238c50b5a4389ee25c1d2
James Trey Nickelsen <jtng28@mst.edu>
2015-09-02 14:20:05 -0500

    [On behalf of Lexmark]: Initialize scalar field in catch_assertionresult.h

--- include/internal/catch_assertionresult.h
+++ include/internal/catch_assertionresult.h
@@ -15,7 +15,7 @@ namespace Catch {
 
     struct AssertionInfo
     {
-        AssertionInfo() {}
+        AssertionInfo() : resultDisposition( ResultDisposition::Normal ) {}
         AssertionInfo(  std::string const& _macroName,
                         SourceLineInfo const& _lineInfo,
                         std::string const& _capturedExpression,
