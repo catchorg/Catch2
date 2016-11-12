b85e1b000e386174d4bd04b51e6dfb4343cdcdf4
GatoRat <joe_woodbury@yahoo.com>
2016-11-11 16:37:58 -0800

    Update catch_capture.hpp
    Added parenthesis around expr to eliminate warnings in GCC 4.4.7

--- include/internal/catch_capture.hpp
+++ include/internal/catch_capture.hpp
@@ -34,7 +34,7 @@
         Catch::ResultBuilder __catchResult( macroName, CATCH_INTERNAL_LINEINFO, #expr, resultDisposition ); \
         try { \
             CATCH_INTERNAL_SUPPRESS_PARENTHESES_WARNINGS \
-            ( __catchResult <= expr ).endExpression(); \
+            ( __catchResult <= (expr) ).endExpression(); \
         } \
         catch( ... ) { \
             __catchResult.useActiveException( Catch::ResultDisposition::Normal ); \
