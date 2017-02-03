712323ab7c0893bd505a56099a7353baa044c59b
Martin Hořeňovský <martin.horenovsky@gmail.com>
2017-02-03 14:09:17 +0100

    Include windows.h proxy header, instead of windows.h itself
    Because the signal changes were in a different branch from the windows.h
    related changes, the SEH handling code included the header directly.
    
    Fixes #803

--- include/internal/catch_fatal_condition.hpp
+++ include/internal/catch_fatal_condition.hpp
@@ -23,11 +23,8 @@ namespace Catch {
 
 #if defined ( CATCH_PLATFORM_WINDOWS ) /////////////////////////////////////////
 
-#define NOMINMAX
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#undef WIN32_LEAN_AND_MEAN
-#undef NOMINMAX
+
+#include "catch_windows_h_proxy.h"
 
 
 namespace Catch {
