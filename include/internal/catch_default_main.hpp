d8230a8d4d5fc4a56d28367791f4440b80438a09
Martin Hořeňovský <martin.horenovsky@gmail.com>
2017-02-16 11:09:09 +0100

    Add opt-in leak detection under MSVC + Windows.
    Closes #439

--- include/internal/catch_default_main.hpp
+++ include/internal/catch_default_main.hpp
@@ -10,8 +10,22 @@
 
 #ifndef __OBJC__
 
+#ifdef CATCH_CONFIG_WINDOWS_CRTDBG
+#include <crtdbg.h>
+#endif
+
 // Standard C/C++ main entry point
 int main (int argc, char * argv[]) {
+#ifdef CATCH_CONFIG_WINDOWS_CRTDBG
+    int flag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+    flag |= _CRTDBG_LEAK_CHECK_DF;
+    flag |= _CRTDBG_ALLOC_MEM_DF;
+    _CrtSetDbgFlag(flag);
+    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
+    // Change this to leaking allocation's number to break there
+    _CrtSetBreakAlloc(-1);
+#endif
     int result = Catch::Session().run( argc, argv );
     return ( result < 0xff ? result : 0xff );
 }
