85aa7707019879e8047eb0945342d8b324d5ae7b
Martin Hořeňovský <martin.horenovsky@gmail.com>
2017-05-08 01:26:06 +0200

    Disable unreachable code warning for TrickyTests.cpp
    Somehow this only triggers for VS 2015, Win32 | Release configuration...

--- projects/SelfTest/TrickyTests.cpp
+++ projects/SelfTest/TrickyTests.cpp
@@ -10,6 +10,11 @@
 #pragma clang diagnostic ignored "-Wpadded"
 #endif
 
+#ifdef _MSC_VER
+#pragma warning (disable : 4702) // Disable unreachable code warning for the last test
+                                 // that is triggered when compiling as Win32|Release
+#endif
+
 #include <stdio.h>
 
 #include "catch.hpp"
