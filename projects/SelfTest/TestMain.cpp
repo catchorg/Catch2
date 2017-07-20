58d8bc6985a70e537cfcc4959dfc405ba9cb2a00
Phil Nash <github@philnash.me>
2017-07-20 23:09:50 +0100

    Removed Text tests (TextFlow has it's own test suite)

--- projects/SelfTest/TestMain.cpp
+++ projects/SelfTest/TestMain.cpp
@@ -232,237 +232,37 @@ TEST_CASE( "Process can be configured on command line", "[config][command-line]"
 }
 
 
-TEST_CASE( "Long strings can be wrapped", "[wrap]" ) {
-
-    using namespace Catch;
-    SECTION( "plain string" ) {
-        // guide:                 123456789012345678
-        std::string testString = "one two three four";
-
-        SECTION( "No wrapping" ) {
-            CHECK( Text( testString, TextAttributes().setWidth( 80 ) ).toString() == testString );
-            CHECK( Text( testString, TextAttributes().setWidth( 18 ) ).toString() == testString );
-        }
-        SECTION( "Wrapped once" ) {
-            CHECK( Text( testString, TextAttributes().setWidth( 17 ) ).toString() == "one two three\nfour" );
-            CHECK( Text( testString, TextAttributes().setWidth( 16 ) ).toString() == "one two three\nfour" );
-            CHECK( Text( testString, TextAttributes().setWidth( 14 ) ).toString() == "one two three\nfour" );
-            CHECK( Text( testString, TextAttributes().setWidth( 13 ) ).toString() == "one two three\nfour" );
-            CHECK( Text( testString, TextAttributes().setWidth( 12 ) ).toString() == "one two\nthree four" );
-        }
-        SECTION( "Wrapped twice" ) {
-            CHECK( Text( testString, TextAttributes().setWidth( 9 ) ).toString() == "one two\nthree\nfour" );
-            CHECK( Text( testString, TextAttributes().setWidth( 8 ) ).toString() == "one two\nthree\nfour" );
-            CHECK( Text( testString, TextAttributes().setWidth( 7 ) ).toString() == "one two\nthree\nfour" );
-        }
-        SECTION( "Wrapped three times" ) {
-            CHECK( Text( testString, TextAttributes().setWidth( 6 ) ).toString() == "one\ntwo\nthree\nfour" );
-            CHECK( Text( testString, TextAttributes().setWidth( 5 ) ).toString() == "one\ntwo\nthree\nfour" );
-        }
-        SECTION( "Short wrap" ) {
-            CHECK( Text( "abcdef", TextAttributes().setWidth( 4 ) ).toString() == "abc-\ndef" );
-            CHECK( Text( "abcdefg", TextAttributes().setWidth( 4 ) ).toString() == "abc-\ndefg" );
-            CHECK( Text( "abcdefgh", TextAttributes().setWidth( 4 ) ).toString() == "abc-\ndef-\ngh" );
-
-            CHECK( Text( testString, TextAttributes().setWidth( 4 ) ).toString() == "one\ntwo\nthr-\nee\nfour" );
-            CHECK( Text( testString, TextAttributes().setWidth( 3 ) ).toString() == "one\ntwo\nth-\nree\nfo-\nur" );
-        }
-        SECTION( "As container" ) {
-            Text text( testString, TextAttributes().setWidth( 6 ) );
-            REQUIRE( text.size() == 4 );
-            CHECK( text[0] == "one" );
-            CHECK( text[1] == "two" );
-            CHECK( text[2] == "three" );
-            CHECK( text[3] == "four" );
-        }
-        SECTION( "Indent first line differently" ) {
-            Text text( testString, TextAttributes()
-                                        .setWidth( 10 )
-                                        .setIndent( 4 )
-                                        .setInitialIndent( 1 ) );
-            CHECK( text.toString() == " one two\n    three\n    four" );
-        }
-
-    }
-
-    SECTION( "With newlines" ) {
-
-        // guide:                 1234567890123456789
-        std::string testString = "one two\nthree four";
-
-        SECTION( "No wrapping" ) {
-            CHECK( Text( testString, TextAttributes().setWidth( 80 ) ).toString() == testString );
-            CHECK( Text( testString, TextAttributes().setWidth( 18 ) ).toString() == testString );
-            CHECK( Text( testString, TextAttributes().setWidth( 10 ) ).toString() == testString );
-        }
-        SECTION( "Trailing newline" ) {
-            CHECK( Text( "abcdef\n", TextAttributes().setWidth( 10 ) ).toString() == "abcdef" );
-            CHECK( Text( "abcdef", TextAttributes().setWidth( 6 ) ).toString() == "abcdef" );
-            CHECK( Text( "abcdef\n", TextAttributes().setWidth( 6 ) ).toString() == "abcdef" );
-            CHECK( Text( "abcdef\n", TextAttributes().setWidth( 5 ) ).toString() == "abcd-\nef" );
-        }
-        SECTION( "Wrapped once" ) {
-            CHECK( Text( testString, TextAttributes().setWidth( 9 ) ).toString() == "one two\nthree\nfour" );
-            CHECK( Text( testString, TextAttributes().setWidth( 8 ) ).toString() == "one two\nthree\nfour" );
-            CHECK( Text( testString, TextAttributes().setWidth( 7 ) ).toString() == "one two\nthree\nfour" );
-        }
-        SECTION( "Wrapped twice" ) {
-            CHECK( Text( testString, TextAttributes().setWidth( 6 ) ).toString() == "one\ntwo\nthree\nfour" );
-        }
-    }
-
-    SECTION( "With wrap-before/ after characters" ) {
-        std::string testString = "one,two(three) <here>";
-
-        SECTION( "No wrapping" ) {
-            CHECK( Text( testString, TextAttributes().setWidth( 80 ) ).toString() == testString );
-            CHECK( Text( testString, TextAttributes().setWidth( 24 ) ).toString() == testString );
-        }
-        SECTION( "Wrap before" ) {
-            CHECK( Text( testString, TextAttributes().setWidth( 11 ) ).toString() == "one,two\n(three)\n<here>" );
-        }
-        SECTION( "Wrap after" ) {
-            CHECK( Text( testString, TextAttributes().setWidth( 6 ) ).toString() == "one,\ntwo\n(thre-\ne)\n<here>" );
-            CHECK( Text( testString, TextAttributes().setWidth( 5 ) ).toString() == "one,\ntwo\n(thr-\nee)\n<her-\ne>" );
-            CHECK( Text( testString, TextAttributes().setWidth( 4 ) ).toString() == "one,\ntwo\n(th-\nree)\n<he-\nre>" );
-        }
-    }
-
-}
-
-using namespace Catch;
-
-class ColourString {
-public:
-
-    struct ColourIndex {
-        ColourIndex( Colour::Code _colour, std::size_t _fromIndex, std::size_t _toIndex )
-        :   colour( _colour ),
-            fromIndex( _fromIndex ),
-            toIndex( _toIndex )
-        {}
-
-        Colour::Code colour;
-        std::size_t fromIndex;
-        std::size_t toIndex;
-    };
-
-    ColourString( std::string const& _string )
-    : string( _string )
-    {}
-    ColourString( std::string const& _string, std::vector<ColourIndex> const& _colours )
-    : string( _string ), colours( _colours )
-    {}
-
-    ColourString& addColour( Colour::Code colour, int _index ) {
-        colours.push_back( ColourIndex( colour,
-                                        resolveRelativeIndex( _index ),
-                                        resolveRelativeIndex( _index )+1 ) );
-        return *this;
-    }
-    ColourString& addColour( Colour::Code colour, int _fromIndex, int _toIndex ) {
-        colours.push_back( ColourIndex( colour,
-                                        resolveRelativeIndex(_fromIndex),
-                                        resolveLastRelativeIndex( _toIndex ) ) );
-        return *this;
-    }
-
-    void writeToStream( std::ostream& _stream ) const {
-        std::size_t last = 0;
-        for( std::size_t i = 0; i < colours.size(); ++i ) {
-            ColourIndex const& index = colours[i];
-            if( index.fromIndex > last )
-                _stream << string.substr( last, index.fromIndex-last );
-            {
-                Colour colourGuard( index.colour );
-                _stream << string.substr( index.fromIndex, index.toIndex-index.fromIndex );
-            }
-            last = index.toIndex;
-        }
-        if( last < string.size() )
-            _stream << string.substr( last );
-    }
-    friend std::ostream& operator << ( std::ostream& _stream, ColourString const& _colourString ) {
-        _colourString.writeToStream( _stream );
-        return _stream;
-    }
-
-private:
-
-    std::size_t resolveLastRelativeIndex( int _index ) {
-        std::size_t index = resolveRelativeIndex( _index );
-        return index == 0 ? string.size() : index;
-    }
-    std::size_t resolveRelativeIndex( int _index ) {
-        return static_cast<std::size_t>( _index >= 0
-            ? _index
-            : static_cast<int>( string.size() )+_index );
-    }
-    std::string string;
-    std::vector<ColourIndex> colours;
-};
-
 TEST_CASE( "replaceInPlace" ) {
     std::string letters = "abcdefcg";
     SECTION( "replace single char" ) {
-        CHECK( replaceInPlace( letters, "b", "z" ) );
+        CHECK( Catch::replaceInPlace( letters, "b", "z" ) );
         CHECK( letters == "azcdefcg" );
     }
     SECTION( "replace two chars" ) {
-        CHECK( replaceInPlace( letters, "c", "z" ) );
+        CHECK( Catch::replaceInPlace( letters, "c", "z" ) );
         CHECK( letters == "abzdefzg" );
     }
     SECTION( "replace first char" ) {
-        CHECK( replaceInPlace( letters, "a", "z" ) );
+        CHECK( Catch::replaceInPlace( letters, "a", "z" ) );
         CHECK( letters == "zbcdefcg" );
     }
     SECTION( "replace last char" ) {
-        CHECK( replaceInPlace( letters, "g", "z" ) );
+        CHECK( Catch::replaceInPlace( letters, "g", "z" ) );
         CHECK( letters == "abcdefcz" );
     }
     SECTION( "replace all chars" ) {
-        CHECK( replaceInPlace( letters, letters, "replaced" ) );
+        CHECK( Catch::replaceInPlace( letters, letters, "replaced" ) );
         CHECK( letters == "replaced" );
     }
     SECTION( "replace no chars" ) {
-        CHECK_FALSE( replaceInPlace( letters, "x", "z" ) );
+        CHECK_FALSE( Catch::replaceInPlace( letters, "x", "z" ) );
         CHECK( letters == letters );
     }
     SECTION( "escape '" ) {
         std::string s = "didn't";
-        CHECK( replaceInPlace( s, "'", "|'" ) );
+        CHECK( Catch::replaceInPlace( s, "'", "|'" ) );
         CHECK( s == "didn|'t" );
     }
-}
-
-// !TBD: This will be folded into Text class
-TEST_CASE( "Strings can be rendered with colour", "[.colour]" ) {
-
-    {
-        ColourString cs( "hello" );
-        cs  .addColour( Colour::Red, 0 )
-            .addColour( Colour::Green, -1 );
-
-        Catch::cout() << cs << std::endl;
-    }
-
-    {
-        ColourString cs( "hello" );
-        cs  .addColour( Colour::Blue, 1, -2 );
-
-        Catch::cout() << cs << std::endl;
-    }
-
-}
-
-TEST_CASE( "Text can be formatted using the Text class" ) {
-
-    CHECK( Text( "hi there" ).toString() == "hi there" );
-
-    TextAttributes narrow;
-    narrow.setWidth( 6 );
-
-    CHECK( Text( "hi there", narrow ).toString() == "hi\nthere" );
 }
 
 
