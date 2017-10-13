da6c2a691477dc7e8cae4a783d1b16d6adb12b21
Phil Nash <github@philnash.me>
2017-10-13 19:44:20 +0100

    Fixed expansion of _FALSE binary expression - see #1051

--- include/internal/catch_assertionresult.hpp
+++ include/internal/catch_assertionresult.hpp
@@ -66,7 +66,7 @@ namespace Catch {
 
     std::string AssertionResult::getExpression() const {
         if( isFalseTest( m_info.resultDisposition ) )
-            return '!' + capturedExpressionWithSecondArgument(m_info.capturedExpression, m_info.secondArg);
+            return "!(" + capturedExpressionWithSecondArgument(m_info.capturedExpression, m_info.secondArg) + ")";
         else
             return capturedExpressionWithSecondArgument(m_info.capturedExpression, m_info.secondArg);
     }
