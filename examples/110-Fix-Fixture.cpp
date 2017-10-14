9544142e66c780a95ab7bc3a4a97a766413d5e08
Martin Moene <martin.moene@gmail.com>
2017-10-14 11:04:23 +0200

    Add traditional fixture example

---
+++ examples/110-Fix-Fixture.cpp
@@ -0,0 +1,69 @@
+// 110-Fix-Fixture.cpp
+
+// Catch has two ways to express fixtures:
+// - Sections (this file)
+// - Traditional class-based fixtures
+
+// Let Catch provide main():
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+class DBConnection
+{
+public:
+    static DBConnection createConnection( std::string const & dbName )
+    {
+        return DBConnection();
+    }
+
+    bool executeSQL( std::string const & query, int const id, std::string const & arg )
+    {
+        if ( arg.length() == 0 )
+        {
+            throw std::logic_error("empty SQL query argument");
+        }
+        return true; // ok
+    }
+};
+
+class UniqueTestsFixture
+{
+private:
+    static int uniqueID;
+
+protected:
+    DBConnection conn;
+
+public:
+    UniqueTestsFixture()
+    : conn( DBConnection::createConnection( "myDB" ) )
+    {}
+
+protected:
+    int getID()
+    {
+        return ++uniqueID;
+    }
+};
+
+int UniqueTestsFixture::uniqueID = 0;
+
+TEST_CASE_METHOD( UniqueTestsFixture, "Create Employee/No Name", "[create]" ) {
+    REQUIRE_THROWS( conn.executeSQL( "INSERT INTO employee (id, name) VALUES (?, ?)", getID(), "") );
+}
+
+TEST_CASE_METHOD( UniqueTestsFixture, "Create Employee/Normal", "[create]" ) {
+    REQUIRE( conn.executeSQL( "INSERT INTO employee (id, name) VALUES (?, ?)", getID(), "Joe Bloggs" ) );
+}
+
+// Compile & run:
+// - g++ -std=c++11 -Wall -I$(CATCH_SINGLE_INCLUDE) -o 110-Fix-Fixture 110-Fix-Fixture.cpp && 110-Fix-Fixture --success
+// - cl -EHsc -I%CATCH_SINGLE_INCLUDE% 110-Fix-Fixture.cpp && 110-Fix-Fixture --success
+
+// Expected compact output (all assertions):
+//
+// prompt> 110-Fix-Fixture.exe --reporter compact --success
+// 110-Fix-Fixture.cpp:53: passed: conn.executeSQL( "INSERT INTO employee (id, name) VALUES (?, ?)", getID(), "")
+// 110-Fix-Fixture.cpp:57: passed: conn.executeSQL( "INSERT INTO employee (id, name) VALUES (?, ?)", getID(), "Joe Bloggs" ) for: true
+// Passed both 2 test cases with 2 assertions.
