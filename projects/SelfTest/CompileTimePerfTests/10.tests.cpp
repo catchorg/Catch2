502da4b38d059fdaa49f6e58486ae512122ef313
Phil Nash <github@philnash.me>
2017-11-15 07:53:19 +0000

    Added files for multiply inclusions of test cases

---
+++ projects/SelfTest/CompileTimePerfTests/10.tests.cpp
@@ -0,0 +1,13 @@
+// Include set of usage tests multiple times - for compile-time performance testing
+// (do not run)
+
+#include "All.tests.cpp"
+#include "All.tests.cpp"
+#include "All.tests.cpp"
+#include "All.tests.cpp"
+#include "All.tests.cpp"
+#include "All.tests.cpp"
+#include "All.tests.cpp"
+#include "All.tests.cpp"
+#include "All.tests.cpp"
+#include "All.tests.cpp"
---
+++ projects/SelfTest/CompileTimePerfTests/100.tests.cpp
@@ -0,0 +1,13 @@
+// Include set of usage tests multiple times - for compile-time performance testing
+// (do not run)
+
+#include "10.tests.cpp"
+#include "10.tests.cpp"
+#include "10.tests.cpp"
+#include "10.tests.cpp"
+#include "10.tests.cpp"
+#include "10.tests.cpp"
+#include "10.tests.cpp"
+#include "10.tests.cpp"
+#include "10.tests.cpp"
+#include "10.tests.cpp"
