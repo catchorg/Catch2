8a7493cd88970a48e7c4b29aa0bfb04206115b61
Phil Nash <github@philnash.me>
2017-11-29 20:01:00 +0300

    Globally suppress Wunused-variable again, for now

--- include/internal/catch_suppress_warnings.h
+++ include/internal/catch_suppress_warnings.h
@@ -10,16 +10,15 @@
 #       pragma warning(push)
 #       pragma warning(disable: 161 1682)
 #   else // __ICC
-#       pragma clang diagnostic push
-#       pragma clang diagnostic ignored "-Wglobal-constructors"
 #       pragma clang diagnostic ignored "-Wunused-variable"
+#       pragma clang diagnostic push
 #       pragma clang diagnostic ignored "-Wpadded"
 #       pragma clang diagnostic ignored "-Wswitch-enum"
 #       pragma clang diagnostic ignored "-Wcovered-switch-default"
 #    endif
 #elif defined __GNUC__
-#    pragma GCC diagnostic push
 #    pragma GCC diagnostic ignored "-Wunused-variable"
+#    pragma GCC diagnostic push
 #    pragma GCC diagnostic ignored "-Wparentheses"
 #    pragma GCC diagnostic ignored "-Wpadded"
 #endif
