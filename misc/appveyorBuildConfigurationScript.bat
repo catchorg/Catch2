a4fd96fbaa8770c03855ef4a625ca86b24f18658
Martin Hořeňovský <martin.horenovsky@gmail.com>
2017-12-03 14:14:58 +0100

    Remove debug prints from batch scripts

--- misc/appveyorBuildConfigurationScript.bat
+++ misc/appveyorBuildConfigurationScript.bat
@@ -3,13 +3,11 @@
 @REM  # 2) Regenerate single header include for examples
 @REM  # 3) Enable building examples
 if "%CONFIGURATION%"=="Debug" (
-  echo "buildConfiguration.bat thinks this is a Debug build"
   python scripts\generateSingleHeader.py
   cmake -Hmisc -Bbuild-misc -A%PLATFORM%
   cmake --build build-misc
   cmake -H. -BBuild -A%PLATFORM% -DUSE_WMAIN=%wmain% -DBUILD_EXAMPLES=ON -DMEMORYCHECK_COMMAND=build-misc\Debug\CoverageHelper.exe -DMEMORYCHECK_COMMAND_OPTIONS=--sep-- -DMEMORYCHECK_TYPE=Valgrind
 )
 if "%CONFIGURATION%"=="Release" (
-  echo "buildConfiguration.bat thinks this is a Release build"
   cmake -H. -BBuild -A%PLATFORM% -DUSE_WMAIN=%wmain%
 )
--- misc/appveyorTestRunScript.bat
+++ misc/appveyorTestRunScript.bat
@@ -1,11 +1,9 @@
 cd Build
 if "%CONFIGURATION%"=="Debug" (
-  echo "appveyorTestScript.bat thinks this is a Debug build"
   ctest -j 2 -C %CONFIGURATION% -D ExperimentalMemCheck
   python ..\misc\appveyorMergeCoverageScript.py
   codecov --root .. --no-color --disable gcov -f cobertura.xml -t %CODECOV_TOKEN%
 )
 if "%CONFIGURATION%"=="Release" (
-  echo "appveyorTestScript.bat thinks this is a Release build"
   ctest -j 2 -C %CONFIGURATION%
 )
