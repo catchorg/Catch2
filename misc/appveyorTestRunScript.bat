dc8a8e6371e7654536d561c21fe6dc9d174e927d
Martin Hořeňovský <martin.horenovsky@gmail.com>
2018-03-06 18:33:18 +0100

    Speed up AppVeyor build times
    * Examples are no longer built for all images
    * Coverage is no longer collected from every build
    * The number of configurations is reduced

--- misc/appveyorTestRunScript.bat
+++ misc/appveyorTestRunScript.bat
@@ -1,8 +1,12 @@
 cd Build
 if "%CONFIGURATION%"=="Debug" (
-  ctest -j 2 -C %CONFIGURATION% -D ExperimentalMemCheck
-  python ..\misc\appveyorMergeCoverageScript.py
-  codecov --root .. --no-color --disable gcov -f cobertura.xml -t %CODECOV_TOKEN%
+  if "%coverage%"=="1" (
+    ctest -j 2 -C %CONFIGURATION% -D ExperimentalMemCheck
+    python ..\misc\appveyorMergeCoverageScript.py
+    codecov --root .. --no-color --disable gcov -f cobertura.xml -t %CODECOV_TOKEN%
+  ) else (
+    ctest -j 2 -C %CONFIGURATION%
+  )
 )
 if "%CONFIGURATION%"=="Release" (
   ctest -j 2 -C %CONFIGURATION%
