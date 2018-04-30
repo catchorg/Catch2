0510d4755f715d21695ea4a5b15069d69789d7a2
Martin Hořeňovský <martin.horenovsky@gmail.com>
2018-04-30 15:15:59 +0200

    Fix missing include and wrong comment format
    While the comment format was valid C++, it breaks our tooling badly.
    I opened up a github issue for our tooling, because unexpected
    formatting of a block comment should not silently generate invalid
    single header file, see #1269.

--- include/internal/catch_output_redirect.cpp
+++ include/internal/catch_output_redirect.cpp
@@ -1,9 +1,9 @@
 /*
-*  Created by Martin on 28/04/2018.
-*
-*  Distributed under the Boost Software License, Version 1.0. (See accompanying
-*  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-*/
+ *  Created by Martin on 28/04/2018.
+ *
+ *  Distributed under the Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
 
 #include "catch_output_redirect.h"
 
@@ -13,6 +13,7 @@
 #include <cstring>
 #include <fstream>
 #include <sstream>
+#include <stdexcept>
 
 #if defined(CATCH_PLATFORM_WINDOWS)
 #include <io.h>      //_dup and _dup2
