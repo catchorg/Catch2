8d5d49299be5b8c62ec90be7d6ee4f71cbe89ea0
Christopher Di Bella <cjdb.ns@gmail.com>
2018-05-05 09:39:55 +0100

    Added GCC 8 to Travis. Updated test so that it warning isn't triggered.

--- projects/SelfTest/UsageTests/Misc.tests.cpp
+++ projects/SelfTest/UsageTests/Misc.tests.cpp
@@ -304,7 +304,7 @@ TEST_CASE( "toString on const wchar_t pointer returns the string contents", "[to
 }
 
 TEST_CASE( "toString on wchar_t const pointer returns the string contents", "[toString]" ) {
-        auto const s = const_cast<wchar_t* const>( L"wide load" );
+        auto const s = const_cast<wchar_t*>( L"wide load" );
         std::string result = ::Catch::Detail::stringify( s );
         CHECK( result == "\"wide load\"" );
 }
