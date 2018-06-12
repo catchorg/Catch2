a19e9bf948e65e1f2025ddf032f9c050a5ecc566
Phil Nash <github@philnash.me>
2018-06-12 23:15:25 +0100

    Some progress. Have a type-erased cache for generators

--- projects/SelfTest/IntrospectiveTests/Generators.tests.cpp
+++ projects/SelfTest/IntrospectiveTests/Generators.tests.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream> // !DBG
 #include <numeric>
+#include <map>
 
 namespace Catch {
 namespace generators {
@@ -67,9 +68,20 @@ namespace generators {
     template<typename Left, typename Right>
     struct ComposedGeneratorsMustHaveTheSameUnderlyingType;
 
+    class GeneratorBase {
+    protected:
+        size_t const m_size;
+
+    public:
+        GeneratorBase( size_t size ) : m_size( size ) {}
+        virtual ~GeneratorBase();
+        auto size() const -> size_t { return m_size; }
+    };
+
+    GeneratorBase::~GeneratorBase() {}
+
     template<typename T>
-    class Generator {
-        size_t m_size;
+    class Generator : public GeneratorBase {
         std::unique_ptr<IGenerator<T>> m_generator;
         std::vector<Generator<T>> m_generators;
 
@@ -81,20 +93,20 @@ namespace generators {
             }
         }
     public:
-        Generator() : m_size( 0 ) {}
+        using type = T;
+
+        Generator() : GeneratorBase( 0 ) {}
 
         Generator( size_t size, std::unique_ptr<IGenerator<T>> generator )
-        :   m_size( size ),
+        :   GeneratorBase( size ),
             m_generator( std::move( generator ) )
         {}
         Generator( Generator&& g1, Generator&& g2 )
-        : m_size( g1.size() + g2.size() ) {
+        : GeneratorBase( g1.size() + g2.size() ) {
             m_generators.reserve( m_size );
             add( std::move( g1 ) );
             add( std::move( g2 ) );
         }
-
-        auto size() const -> size_t { return m_size; }
 
         auto operator[]( size_t index ) const -> T {
             assert( index < m_size );
@@ -128,6 +140,45 @@ namespace generators {
             return ComposedGeneratorsMustHaveTheSameUnderlyingType<T, U>();
         }
     };
+
+    using GeneratorBasePtr = std::unique_ptr<GeneratorBase>;
+
+    class GeneratorCache {
+        std::map<std::string, GeneratorBasePtr> m_generators;
+
+    public:
+        template<typename T>
+        auto add( std::string const& id, Generator<T>&& generator ) -> Generator<T> const& {
+            auto generatorPtr = make_unique<Generator<T>>( std::move( generator ) );
+            auto const& storedGenerator = *generatorPtr;
+            m_generators.insert( { id, std::move( generatorPtr ) } );
+            return storedGenerator;
+        }
+
+        template<typename T, typename L>
+        auto getGenerator( std::string const& id, L const& generatorExpression ) -> Generator<T> const& {
+
+            auto it = m_generators.find( id );
+            return ( it != m_generators.end() )
+                ? static_cast<Generator<T> const&>( *it->second )
+                : add( id, generatorExpression() );
+        }
+
+    };
+//    auto getGeneratorCache() -> GeneratorCache& {
+//        static GeneratorCache s_cache;
+//        return s_cache;
+//    }
+
+    template<typename L>
+    auto generate( GeneratorCache& cache, std::string const& id, size_t index, L const& generatorExpression ) -> typename decltype(generatorExpression())::type {
+
+        using UnderlyingType = typename decltype(generatorExpression())::type;
+
+        auto const& generator = cache.getGenerator<UnderlyingType, L>( id, generatorExpression );
+
+        return generator[index];
+    }
 
 
     template<typename T>
@@ -189,6 +240,51 @@ TEST_CASE("Generators") {
 
         CHECK( gen.size() == 1 );
         CHECK( gen[0] == 7 );
+    }
+
+    SECTION( "type erasure" ) {
+        auto gen = range( 7, 9 ) << 11;
+
+        // Make type erased version
+        auto dynCopy = make_unique<Generator<int>>( std::move( gen ) );
+        std::unique_ptr<GeneratorBase const> base = std::move( dynCopy );
+
+        // Only thing we can do is ask for the size
+        CHECK( base->size() == 4 );
+
+        // Restore typed version
+        auto typed = dynamic_cast<Generator<int> const*>( base.get() );
+        REQUIRE( typed );
+        CHECK( typed->size() == 4 );
+        CHECK( (*typed)[0] == 7 );
+        CHECK( (*typed)[3] == 11 );
+    }
+
+    SECTION( "memoized" ) {
+        GeneratorCache cache;
+
+        std::string id = "test";
+
+        SECTION( "type is deducible" ) {
+            using GeneratorType = decltype( range( 11, 14 ) << 27 );
+            REQUIRE( std::is_same<GeneratorType, Generator<int>>::value );
+            REQUIRE( std::is_same<GeneratorType::type, int>::value );
+        }
+
+        SECTION( "macro2" ) {
+            int created = 0;
+            auto fun = [&]{
+                created++;
+                return values({42, 7});
+            };
+
+            CHECK( generate( cache, id, 0, fun ) == 42 );
+            CHECK( created == 1 );
+            CHECK( generate( cache, id, 0, fun ) == 42 );
+            CHECK( created == 1 );
+            CHECK( generate( cache, id, 1, fun ) == 7 );
+            CHECK( created == 1 );
+        }
     }
 
     //range( 1, 2 ) << values( { 3.1, 7.9 } ); // should error
