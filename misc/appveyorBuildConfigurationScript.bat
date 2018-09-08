1e98c820bbc6712e0e9bec4dc63f9b1cd83dee78
Martin Hořeňovský <martin.horenovsky@gmail.com>
2018-09-08 20:15:51 +0200

    Simplify the Appveyor configuration batch script

--- misc/appveyorBuildConfigurationScript.bat
+++ misc/appveyorBuildConfigurationScript.bat
@@ -13,14 +13,8 @@ if "%CONFIGURATION%"=="Debug" (
     cmake -H. -BBuild -A%PLATFORM% -DUSE_WMAIN=%wmain% -DMEMORYCHECK_COMMAND=build-misc\Debug\CoverageHelper.exe -DMEMORYCHECK_COMMAND_OPTIONS=--sep-- -DMEMORYCHECK_TYPE=Valgrind || exit /b !ERRORLEVEL! || exit /b !ERRORLEVEL!
   ) else (
     @REM # We know that coverage is 0
-    if "%examples%"=="1" (
-      @REM # Examples live off the single header, so it needs to be regenerated
-      python scripts\generateSingleHeader.py || exit /b !ERRORLEVEL!
-      cmake -H. -BBuild -A%PLATFORM% -DUSE_WMAIN=%wmain% -DCATCH_BUILD_EXAMPLES=ON -DCATCH_BUILD_EXTRA_TESTS=ON || exit /b !ERRORLEVEL!
-    ) else (
-      @REM # This is just a plain debug build
-      cmake -H. -BBuild -A%PLATFORM% -DUSE_WMAIN=%wmain% || exit /b !ERRORLEVEL!
-    )
+    python scripts\generateSingleHeader.py || exit /b !ERRORLEVEL!
+    cmake -H. -BBuild -A%PLATFORM% -DUSE_WMAIN=%wmain% -DCATCH_BUILD_EXAMPLES=%examples% -DCATCH_BUILD_EXTRA_TESTS=%examples% || exit /b !ERRORLEVEL!
   )
 )
 if "%CONFIGURATION%"=="Release" (
