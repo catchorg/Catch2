8989c9b560f14a4ddbadbf564499d732bed8b264
Martin Hořeňovský <martin.horenovsky@gmail.com>
2019-01-15 23:13:16 +0100

    Integrate tests for #1394 into our test suite

--- examples/runFilteredSection.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# make sure single-file header was regenerated
-../scripts/generateSingleHeader.py
-
-# run Test case with specified section to trigger bug #1394
-../cmake-build-debug/examples/300-FilteredSection \#1394 -c RunSection
-../cmake-build-debug/examples/300-FilteredSection \#1394\ nested -c NestedRunSection -c s1
