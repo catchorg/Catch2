d96f5a2c78e977bc217f6e6a46fe5a9fdd5d2c49
hbina4326 <hanif.ariffin.4326@gmail.com>
2019-01-24 15:35:18 -0500

    fixed a major problem with the code...

--- include/internal/catch_timer.cpp
+++ include/internal/catch_timer.cpp
@@ -9,6 +9,7 @@
 #include "catch_timer.h"
 #include <vector>
 #include <chrono>
+#include <assert.h>
 
 static const uint64_t nanosecondsInSecond = 1000000000;
 static const uint64_t STD_DEVIATION_THRESHOLD = 100;
@@ -23,7 +24,7 @@ namespace Catch {
     namespace {
         auto estimateClockResolution() -> uint64_t {
             uint64_t result = 0;
-            bool std_deviation_is_stable = false;
+            int attempts = 0;
             do {
                 uint64_t sum = 0;
                 std::vector<uint64_t> deltas;
@@ -42,26 +43,32 @@ namespace Catch {
                     auto delta = ticks - baseTicks;
                     deltas.push_back(delta);
                     sum += delta;
-                // If we have been calibrating for over 3 seconds -- the clock
-                // is terrible and we should move on.
-                // TBD: How to signal that the measured resolution is probably wrong?
-                if (ticks > startTime + 3 * nanosecondsInSecond) {
-                    return sum / ( i + 1u );
-                }
+                    // If we have been calibrating for over 3 seconds -- the clock
+                    // is terrible and we should move on.
+                    // TBD: How to signal that the measured resolution is probably wrong?
+                    if (ticks > startTime + 3 * nanosecondsInSecond) {
+                        return sum / (i + 1u);
+                    }
 
-                // We're just taking the mean, here. To do better we could take the std. dev and exclude outliers
-                // - and potentially do more iterations if there's a high variance.
-                result = sum / iterations;
-                auto squared_difference_sum = 0;
-                for (std::size_t iterator = 0; iterator < iterations; ++iterator) {
-                    squared_difference_sum += (deltas[iterator] - result) * (deltas[iterator] - result);
+                    // We're just taking the mean, here. To do better we could take the std. dev and exclude outliers
+                    // - and potentially do more iterations if there's a high variance.
+                    result = sum / iterations;
+                    auto squared_difference_sum = 0;
+                    for (std::size_t iterator = 0; iterator < iterations; ++iterator) {
+                        squared_difference_sum += (deltas[iterator] - result) * (deltas[iterator] - result);
+                    }
+                    auto std_deviation = squared_difference_sum / iterations;
+                    if (std_deviation < STD_DEVIATION_THRESHOLD) {
+                        return result;
+                    } else {
+                        attempts++;
+
+                    }
                 }
-                auto std_deviation = squared_difference_sum / iterations;
-                if (std_deviation < STD_DEVIATION_THRESHOLD) {
-                    std_deviation_is_stable = true;
-                }
-            } while (!std_deviation_is_stable);
-            return result;
+            } while (attempts < 10);
+            // too much attempts have failed, exit program. should i assert(false) here or tell user that their computer is unreliable for benchmarking
+
+            return 0u;
         }
     }
 
