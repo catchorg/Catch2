865c2e2596429323f653c9ce71224a1891559f4b
Petr Ledvina <ledvinap@gmail.com>
2019-04-25 11:18:01 +0200

    Add test case for string / char in capture

--- projects/SelfTest/UsageTests/Message.tests.cpp
+++ projects/SelfTest/UsageTests/Message.tests.cpp
@@ -251,6 +251,13 @@ TEST_CASE("CAPTURE can deal with complex expressions involving commas", "[messag
     SUCCEED();
 }
 
+TEST_CASE("CAPTURE parses string and character constants", "[messages][capture]") {
+    CAPTURE(("comma, in string", "escaped, \", "), "single quote in string,',", "some escapes, \\,\\\\");
+    CAPTURE("some, ), unmatched, } prenheses {[<");
+    CAPTURE('"', '\'', ',', '}', ')', '(', '{');
+    SUCCEED();
+}
+
 #ifdef __clang__
 #pragma clang diagnostic pop
 #endif
