fb74bb133ccc8b1bbf932071f3db039861dea73a
Martin Hořeňovský <martin.horenovsky@gmail.com>
2019-07-20 21:06:56 +0200

    Fix coverage report merging
    For some time now (I'd guess almost a year :shrug:), the coverage
    merging on Windows has been failing, because the reports have been
    generated in a different folder than expected. Our merge script did
    not report failure because it was not checking the returned error
    code from OpenCppCoverage, and for some reason, the `codecov` tool
    happily returned 0 even though it did not find the file it was
    supposed to upload...
    
    The former is also fixed by this commit.

--- misc/appveyorMergeCoverageScript.py
+++ misc/appveyorMergeCoverageScript.py
@@ -4,6 +4,6 @@ import glob
 import subprocess
 
 if __name__ == '__main__':
-    cov_files = list(glob.glob('cov-report*.bin'))
+    cov_files = list(glob.glob('projects/cov-report*.bin'))
     base_cmd = ['OpenCppCoverage', '--quiet', '--export_type=cobertura:cobertura.xml'] + ['--input_coverage={}'.format(f) for f in cov_files]
-    subprocess.call(base_cmd)
+    subprocess.check_call(base_cmd)
