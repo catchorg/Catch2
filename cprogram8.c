1c39d1bae8eff39e5ac26bbf66788861d36dcaa7
Preetha2019 <57024934+Preetha2019@users.noreply.github.com>
2019-10-29 17:42:33 +0530

    Create cprogram8.c

---
+++ cprogram8.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+unsigned int Factorial( unsigned int number );
+int main()
+{
+    unsigned int number,f,i;
+    printf("\nEnter an Integer:");
+    scanf("%d", &number);
+    f= Factorial(number);
+     printf("Factorial of %d is:%d", number, f);
+     return(0);
+}
+    unsigned int Factorial( unsigned int number )
+    {
+        unsigned int i;
+        if(n<0)
+        {
+            printf
+        }
+        for(i=1; i<=number; i++)
+        {
+            return number > 1 ? Factorial(number-1)*number : 1;
+        }
+    }
+
