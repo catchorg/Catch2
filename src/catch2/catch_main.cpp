0c62a503921817f1a161270b835fe70a797041bc
Martin Hořeňovský <martin.horenovsky@gmail.com>
2019-12-18 18:02:57 +0100

    Add the catch_main source file

---
+++ src/catch2/catch_main.cpp
@@ -0,0 +1,6 @@
+/*
+ *  Distributed under the Boost Software License, Version 1.0. (See accompanying
+ *  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#include <catch2/catch_default_main.hpp>
