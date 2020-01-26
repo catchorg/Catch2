bbcd2e73e15aadc3e47a51a451f0b0ed0a467093
Martin Hořeňovský <martin.horenovsky@gmail.com>
2020-01-26 09:42:32 +0100

    Remove C++11 compat

--- src/catch2/catch_matchers_templates.hpp
+++ src/catch2/catch_matchers_templates.hpp
@@ -40,38 +40,9 @@ namespace Matchers {
         #endif // CATCH_CPP17_OR_GREATER
 
 
-        #ifdef CATCH_CPP14_OR_GREATER
-
         using std::index_sequence;
         using std::index_sequence_for;
         using std::make_index_sequence;
-
-        #else // CATCH_CPP14_OR_GREATER
-
-        /* C++14 index_sequence for C++11 */
-
-        template<typename T, T... Ints>
-        struct integer_sequence {
-            using value_type = T;
-            static constexpr std::size_t size() { return sizeof...(Ints); }
-        };
-
-        template<std::size_t... Ints>
-        using index_sequence = integer_sequence<std::size_t, Ints...>;
-
-        template<typename T, std::size_t N, T... Is>
-        struct make_integer_sequence : make_integer_sequence<T, N-1, N-1, Is...> {};
-
-        template<typename T, T... Is>
-        struct make_integer_sequence<T, 0, Is...> : integer_sequence<T, Is...> {};
-
-        template<std::size_t N>
-        using make_index_sequence = make_integer_sequence<std::size_t, N>;
-
-        template<typename... T>
-        using index_sequence_for = make_index_sequence<sizeof...(T)>;
-
-        #endif // CATCH_CPP14_OR_GREATER
 
 
         template<typename T>
