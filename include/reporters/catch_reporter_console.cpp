3a15315a37c16a5cebee30788a19023f85b9f401
Till Hofmann <hofmann@kbsg.rwth-aachen.de>
2020-02-10 14:52:35 +0100

    Switch back to uncolored output after printing filters
    After printing the list of filters, switch back from yellow to black
    before printing a newline to avoid the remaining output to be colored in
    yellow.

--- include/reporters/catch_reporter_console.cpp
+++ include/reporters/catch_reporter_console.cpp
@@ -680,8 +680,11 @@ void ConsoleReporter::printSummaryDivider() {
 }
 
 void ConsoleReporter::printTestFilters() {
-    if (m_config->testSpec().hasFilters())
-        stream << Colour(Colour::BrightYellow) << "Filters: " << serializeFilters( m_config->getTestsOrTags() ) << '\n';
+    if (m_config->testSpec().hasFilters()) {
+        stream << Colour(Colour::BrightYellow)
+               << "Filters: " << serializeFilters(m_config->getTestsOrTags())
+               << Colour(Colour::None) << '\n';
+    }
 }
 
 CATCH_REGISTER_REPORTER("console", ConsoleReporter)
