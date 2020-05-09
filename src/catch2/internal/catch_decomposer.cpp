eef6c9b79b43a4f5a902813093266f5a8633eb60
Martin Hořeňovský <martin.horenovsky@gmail.com>
2020-05-09 21:02:12 +0200

    Microopt: stream single char instead of single char string

--- src/catch2/internal/catch_decomposer.cpp
+++ src/catch2/internal/catch_decomposer.cpp
@@ -12,13 +12,13 @@
 namespace Catch {
 
     ITransientExpression::~ITransientExpression() = default;
-    
+
     void formatReconstructedExpression( std::ostream &os, std::string const& lhs, StringRef op, std::string const& rhs ) {
         if( lhs.size() + rhs.size() < 40 &&
                 lhs.find('\n') == std::string::npos &&
                 rhs.find('\n') == std::string::npos )
-            os << lhs << " " << op << " " << rhs;
+            os << lhs << ' ' << op << ' ' << rhs;
         else
-            os << lhs << "\n" << op << "\n" << rhs;
+            os << lhs << '\n' << op << '\n' << rhs;
     }
 }
