90d6fd849ee034fee5caa25f51c7ca107c9e6eca
Martin Hořeňovský <martin.horenovsky@gmail.com>
2020-07-07 11:36:51 +0200

    Increase tolerances in --min-duration tests
    The underpowered and oversubscribed CI servers are hell.

--- tests/SelfTest/TimingTests/Sleep.tests.cpp
+++ tests/SelfTest/TimingTests/Sleep.tests.cpp
@@ -16,8 +16,8 @@ TEST_CASE( "sleep_for_100ms", "[.min_duration_test][approvals]" )
   CHECK( true );
 }
 
-TEST_CASE( "sleep_for_200ms", "[.min_duration_test][approvals]" )
+TEST_CASE( "sleep_for_250ms", "[.min_duration_test][approvals]" )
 {
-  std::this_thread::sleep_for( std::chrono::milliseconds( 200 ) );
+  std::this_thread::sleep_for( std::chrono::milliseconds( 250 ) );
   CHECK( true );
 }
