fd9f5ac661f87335ecd70d39849c1d3a90f1c64d
Martin Hořeňovský <martin.horenovsky@gmail.com>
2020-09-07 12:34:55 +0200

    v2.13.1

--- include/catch.hpp
+++ include/catch.hpp
@@ -11,7 +11,7 @@
 
 #define CATCH_VERSION_MAJOR 2
 #define CATCH_VERSION_MINOR 13
-#define CATCH_VERSION_PATCH 0
+#define CATCH_VERSION_PATCH 1
 
 #ifdef __clang__
 #    pragma clang system_header
--- include/internal/catch_version.cpp
+++ include/internal/catch_version.cpp
@@ -37,7 +37,7 @@ namespace Catch {
     }
 
     Version const& libraryVersion() {
-        static Version version( 2, 13, 0, "", 0 );
+        static Version version( 2, 13, 1, "", 0 );
         return version;
     }
 
