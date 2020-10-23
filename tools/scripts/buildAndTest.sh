11f6e8f04b65f588ea0d5755b9b0be048318547e
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-10-23 19:47:05 +0100

    Embed the script buildAndTest.sh inside the docs

--- tools/scripts/buildAndTest.sh
+++ tools/scripts/buildAndTest.sh
@@ -3,6 +3,7 @@
 # Start at the root of the Catch project directory, for example:
 # cd Catch2
 
+# begin-snippet: catch2-build-and-test
 # 1. Regenerate the amalgamated distribution
 ./tools/scripts/generateAmalgamatedFiles.py
 
@@ -15,3 +16,4 @@ cmake --build debug-build
 # 4. Run the tests using CTest
 cd debug-build
 ctest -j 4 --output-on-failure -C Debug
+# end-snippet
