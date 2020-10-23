4035beb98848153b535ddfa746271ee2d4658699
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-10-23 19:40:42 +0100

    Add a script - buildAndTest.sh
    This contains the currently-documented steps for a developer build.
    Also, make git ignore its output directory.

---
+++ tools/scripts/buildAndTest.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# Start at the root of the Catch project directory, for example:
+# cd Catch2
+
+# 1. Regenerate the amalgamated distribution
+./tools/scripts/generateAmalgamatedFiles.py
+
+# 2. Configure the full test build
+cmake -Bdebug-build -H. -DCMAKE_BUILD_TYPE=Debug -DCATCH_BUILD_EXAMPLES=ON -DCATCH_BUILD_EXTRA_TESTS=ON
+
+# 3. Run the actual build
+cmake --build debug-build
+
+# 4. Run the tests using CTest
+cd debug-build
+ctest -j 4 --output-on-failure -C Debug
