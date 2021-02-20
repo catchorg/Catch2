3afea8128a060888db8d5547fd35400c7ecb05e4
Martin Hořeňovský <martin.horenovsky@gmail.com>
2021-02-20 23:08:58 +0100

    Increase timing window for min duration tests to 1s
    This test tends to be brittle on Mac CI machines, which are
    heavily loaded and bursty. Since the tests are only run as part
    of the "extra tests" test set, this increase should not have
    a significant impact on the total duration of CI runs.

--- tests/SelfTest/TimingTests/Sleep.tests.cpp
+++ tests/SelfTest/TimingTests/Sleep.tests.cpp
@@ -16,8 +16,8 @@ TEST_CASE( "sleep_for_100ms", "[.min_duration_test][approvals]" )
   CHECK( true );
 }
 
-TEST_CASE( "sleep_for_250ms", "[.min_duration_test][approvals]" )
+TEST_CASE( "sleep_for_1000ms", "[.min_duration_test][approvals]" )
 {
-  std::this_thread::sleep_for( std::chrono::milliseconds( 250 ) );
+  std::this_thread::sleep_for( std::chrono::milliseconds( 1'000 ) );
   CHECK( true );
 }
