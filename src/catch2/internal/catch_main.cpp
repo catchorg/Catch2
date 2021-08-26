07018e2fba61234b54dfd2633c2501a911938614
Martin Hořeňovský <martin.horenovsky@gmail.com>
2021-08-26 23:13:27 +0200

    Force __cdecl calling convention for wmain
    Fixes #2219

--- src/catch2/internal/catch_main.cpp
+++ src/catch2/internal/catch_main.cpp
@@ -20,7 +20,7 @@ namespace Catch {
 
 #if defined(CATCH_CONFIG_WCHAR) && defined(CATCH_PLATFORM_WINDOWS) && defined(_UNICODE) && !defined(DO_NOT_USE_WMAIN)
 // Standard C/C++ Win32 Unicode wmain entry point
-extern "C" int wmain (int argc, wchar_t * argv[], wchar_t * []) {
+extern "C" int __cdecl wmain (int argc, wchar_t * argv[], wchar_t * []) {
 #else
 // Standard C/C++ main entry point
 int main (int argc, char * argv[]) {
