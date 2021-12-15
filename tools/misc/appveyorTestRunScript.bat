d30d0c01a7505cd27a8ef718bbd50d90b36363c4
Alan Jowett <alanjo@microsoft.com>
2021-12-15 15:40:35 -0700

    Disable JIT debug for ctest.ext
    Signed-off-by: Alan Jowett <alanjo@microsoft.com>

--- tools/misc/appveyorTestRunScript.bat
+++ tools/misc/appveyorTestRunScript.bat
@@ -1,5 +1,7 @@
 SETLOCAL EnableDelayedExpansion
 
+rem Disable launching the JIT debugger for ctest.exe
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\AeDebug\AutoExclusionList" /v "ctest.ext" /t REG_DWORD /d 1
 cd Build
 if "%CONFIGURATION%"=="Debug" (
   if "%coverage%"=="1" (
