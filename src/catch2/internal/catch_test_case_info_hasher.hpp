6206db5a73e989965d7f67b710762ab91c2c0959
Martin Hořeňovský <martin.horenovsky@gmail.com>
2022-03-31 20:04:08 +0200

    Fix missing licence headers

--- src/catch2/internal/catch_test_case_info_hasher.hpp
+++ src/catch2/internal/catch_test_case_info_hasher.hpp
@@ -1,3 +1,10 @@
+
+//              Copyright Catch2 Authors
+// Distributed under the Boost Software License, Version 1.0.
+//   (See accompanying file LICENSE_1_0.txt or copy at
+//        https://www.boost.org/LICENSE_1_0.txt)
+
+// SPDX-License-Identifier: BSL-1.0
 #ifndef CATCH_TEST_CASE_INFO_HASHER_HPP_INCLUDED
 #define CATCH_TEST_CASE_INFO_HASHER_HPP_INCLUDED
 
