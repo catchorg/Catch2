12b43901691bfc2b119bdda61e3cd4f774d3bd05
Martin Hořeňovský <martin.horenovsky@gmail.com>
2022-10-28 14:39:51 +0200

    Fix license rebase error in test helpers

--- tests/SelfTest/helpers/parse_test_spec.hpp
+++ tests/SelfTest/helpers/parse_test_spec.hpp
@@ -1,10 +1,13 @@
 
 //              Copyright Catch2 Authors
 // Distributed under the Boost Software License, Version 1.0.
-//   (See accompanying file LICENSE_1_0.txt or copy at
+//   (See accompanying file LICENSE.txt or copy at
 //        https://www.boost.org/LICENSE_1_0.txt)
 
 // SPDX-License-Identifier: BSL-1.0
+
+#ifndef CATCH_TEST_HELPERS_PARSE_TEST_SPEC_HPP_INCLUDED
+#define CATCH_TEST_HELPERS_PARSE_TEST_SPEC_HPP_INCLUDED
 
 #include <catch2/catch_test_spec.hpp>
 
@@ -13,3 +16,5 @@
 namespace Catch {
     TestSpec parseTestSpec( std::string const& arg );
 }
+
+#endif // CATCH_TEST_HELPERS_PARSE_TEST_SPEC_HPP_INCLUDED
