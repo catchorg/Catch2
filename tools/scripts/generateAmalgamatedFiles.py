932508f206cdadf7700fa051eca2944b0f57a785
Ryan Pavlik <ryan.pavlik@collabora.com>
2023-08-11 15:06:03 -0500

    Edited amalgamated file generator, to block REUSE from getting confused
    It struggled with the copyright lines included as string literals.

--- tools/scripts/generateAmalgamatedFiles.py
+++ tools/scripts/generateAmalgamatedFiles.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+#              Copyright Catch2 Authors
+# Distributed under the Boost Software License, Version 1.0.
+#   (See accompanying file LICENSE.txt or copy at
+#        https://www.boost.org/LICENSE_1_0.txt)
+# SPDX-License-Identifier: BSL-1.0
 
 import os
 import re
@@ -11,6 +16,8 @@ root_path = os.path.join(catchPath, 'src')
 starting_header = os.path.join(root_path, 'catch2', 'catch_all.hpp')
 output_header = os.path.join(catchPath, 'extras', 'catch_amalgamated.hpp')
 output_cpp = os.path.join(catchPath, 'extras', 'catch_amalgamated.cpp')
+
+# REUSE-IgnoreStart
 
 # These are the copyright comments in each file, we want to ignore them
 copyright_lines = [
@@ -38,6 +45,8 @@ file_header = '''\
 //  You probably shouldn't edit it directly.
 //  ----------------------------------------------------------
 '''
+
+# REUSE-IgnoreEnd
 
 # Returns file header with proper version string and generation time
 def formatted_file_header(version):
