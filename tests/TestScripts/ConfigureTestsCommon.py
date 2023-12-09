e4e7376d789e1165e00e2b7c8b13e02c8118a414
danildenha <danildenha@gmail.com>
2023-12-09 00:43:57 -0600

    Update ConfigureTestsCommon.py
    Instead of printing error messages directly to the console, it uses logging and providing more informative error messages that can help identify the issue. Consistent usage of single -double quotes

--- tests/TestScripts/ConfigureTestsCommon.py
+++ tests/TestScripts/ConfigureTestsCommon.py
@@ -8,68 +8,53 @@
 # SPDX-License-Identifier: BSL-1.0
 
 from typing import List, Tuple
-
 import os
 import subprocess
+import logging
 
 def configure_and_build(source_path: str, project_path: str, options: List[Tuple[str, str]]):
-    base_configure_cmd = ['cmake',
-                          '-B{}'.format(project_path),
-                          '-H{}'.format(source_path),
-                          '-DCMAKE_BUILD_TYPE=Debug',
-                          '-DCATCH_DEVELOPMENT_BUILD=ON']
+    configure_cmd = ['cmake', '-B{}'.format(project_path), '-H{}'.format(source_path),
+                     '-DCMAKE_BUILD_TYPE=Debug', '-DCATCH_DEVELOPMENT_BUILD=ON']
     for option, value in options:
-        base_configure_cmd.append('-D{}={}'.format(option, value))
-    try:
-        subprocess.run(base_configure_cmd,
-                       stdout = subprocess.PIPE,
-                       stderr = subprocess.STDOUT,
-                       check = True)
-    except subprocess.SubprocessError as ex:
-        print("Could not configure build to '{}' from '{}'".format(project_path, source_path))
-        print("Return code: {}".format(ex.returncode))
-        print("output: {}".format(ex.output))
-        raise
-    print('Configuring {} finished'.format(project_path))
+        configure_cmd.append('-D{}={}'.format(option, value))
 
-    build_cmd = ['cmake',
-                 '--build', '{}'.format(project_path),
-                 # For now we assume that we only need Debug config
-                 '--config', 'Debug']
     try:
-        subprocess.run(build_cmd,
-                       stdout = subprocess.PIPE,
-                       stderr = subprocess.STDOUT,
-                       check = True)
-    except subprocess.SubprocessError as ex:
-        print("Could not build project in '{}'".format(project_path))
-        print("Return code: {}".format(ex.returncode))
-        print("output: {}".format(ex.output))
+        subprocess.run(configure_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True)
+    except subprocess.CalledProcessError as ex:
+        logging.error(f"Could not configure build to '{project_path}' from '{source_path}'")
+        logging.error(f"Return code: {ex.returncode}")
+        logging.error(f"output: {ex.output}")
         raise
-    print('Building {} finished'.format(project_path))
+
+    print(f'Configuring {project_path} finished')
+
+    build_cmd = ['cmake', '--build', '{}'.format(project_path), '--config', 'Debug']
+    try:
+        subprocess.run(build_cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True)
+    except subprocess.CalledProcessError as ex:
+        logging.error(f"Could not build project in '{project_path}'")
+        logging.error(f"Return code: {ex.returncode}")
+        logging.error(f"output: {ex.output}")
+        raise
+
+    print(f'Building {project_path} finished')
 
 def run_and_return_output(base_path: str, binary_name: str, other_options: List[str]) -> Tuple[str, str]:
-    # For now we assume that Windows builds are done using MSBuild under
-    # Debug configuration. This means that we need to add "Debug" folder
-    # to the path when constructing it. On Linux, we don't add anything.
     config_path = "Debug" if os.name == 'nt' else ""
     full_path = os.path.join(base_path, config_path, binary_name)
 
-    base_cmd = [full_path]
-    base_cmd.extend(other_options)
+    cmd = [full_path]
+    cmd.extend(other_options)
 
     try:
-        ret = subprocess.run(base_cmd,
-                             stdout = subprocess.PIPE,
-                             stderr = subprocess.PIPE,
-                             check = True,
-                             universal_newlines = True)
-    except subprocess.SubprocessError as ex:
-        print('Could not run "{}"'.format(base_cmd))
-        print('Args: "{}"'.format(other_options))
-        print('Return code: {}'.format(ex.returncode))
-        print('stdout: {}'.format(ex.stdout))
-        print('stderr: {}'.format(ex.stdout))
+        ret = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True, universal_newlines=True)
+    except subprocess.CalledProcessError as ex:
+        logging.error(f'Could not run "{cmd}"')
+        logging.error(f'Args: "{other_options}"')
+        logging.error(f'Return code: {ex.returncode}')
+        logging.error(f'stdout: {ex.stdout}')
+        logging.error(f'stderr: {ex.stdout}')
         raise
 
-    return (ret.stdout, ret.stderr)
+    return ret.stdout, ret.stderr
+
