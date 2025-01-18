a7028d2a9ea8faddd7d04bc9f6f1c65d80bb0474
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2025-01-18 11:29:04 -0600

    Rewrite multithreading test to not use jthreads

--- tests/ExtraTests/X37-Multithreading.cpp
+++ tests/ExtraTests/X37-Multithreading.cpp
@@ -11,25 +11,27 @@
 #include <catch2/benchmark/catch_benchmark.hpp>
 
 #include <thread>
-#include <stop_token>
+#include <atomic>
 
 TEST_CASE( "ThreadAssertionTest",
            "[Multithreading]" ) {
+    std::atomic_bool should_stop = false;
     SECTION( "Basic" ) {
-        std::jthread a([] (const std::stop_token& token) {
-            while (!token.stop_requested()) {
+        std::thread a([&should_stop] () {
+            while (!should_stop) {
                 FAIL_CHECK(false);
                 CHECK(true);
             }
         });
-        std::jthread b([] (const std::stop_token& token) {
-            while (!token.stop_requested()) {
+        std::thread b([&should_stop] () {
+            while (!should_stop) {
                 FAIL_CHECK(false);
                 CHECK(true);
             }
         });
         std::this_thread::sleep_for( std::chrono::milliseconds( 1'000 ) );
-        a.get_stop_source().request_stop();
-        b.get_stop_source().request_stop();
+        should_stop = true;
+        a.join();
+        b.join();
     }
 }
