5a0e434a2e4cd9011c10588594c220536f380a03
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2025-01-18 10:47:32 -0600

    Suppress -Wexit-time-destructors

--- src/catch2/internal/catch_global_lock.cpp
+++ src/catch2/internal/catch_global_lock.cpp
@@ -7,8 +7,17 @@
 // SPDX-License-Identifier: BSL-1.0
 #include <catch2/internal/catch_global_lock.hpp>
 
+#if defined(__clang__)
+#    pragma clang diagnostic push
+#    pragma clang diagnostic ignored "-Wexit-time-destructors"
+#endif
+
 namespace Catch {
 
     std::recursive_mutex global_lock;
 
 } // namespace Catch
+
+#if defined(__clang__)
+#    pragma clang diagnostic pop
+#endif
