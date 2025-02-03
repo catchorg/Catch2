5ed4b70e2273eff469d7d1dca5fec089223768de
Pavlo Kleymonov <pkleymonov@blackberry.com>
2025-02-03 16:14:41 +0100

    reverted modification of amalgamated distribution files

--- extras/catch_amalgamated.cpp
+++ extras/catch_amalgamated.cpp
@@ -3509,7 +3509,7 @@ namespace {
 #endif // Windows/ ANSI/ None
 
 
-#if defined( CATCH_PLATFORM_LINUX ) || defined( CATCH_PLATFORM_MAC ) || defined( CATCH_PLATFORM_QNX )
+#if defined( CATCH_PLATFORM_LINUX ) || defined( CATCH_PLATFORM_MAC )
 #    define CATCH_INTERNAL_HAS_ISATTY
 #    include <unistd.h>
 #endif
@@ -3757,7 +3757,7 @@ namespace Catch {
         #endif
     } // namespace Catch
 
-#elif defined(CATCH_PLATFORM_LINUX) || defined( CATCH_PLATFORM_QNX )
+#elif defined(CATCH_PLATFORM_LINUX)
     #include <fstream>
     #include <string>
 
--- extras/catch_amalgamated.hpp
+++ extras/catch_amalgamated.hpp
@@ -101,9 +101,6 @@
 #elif defined(linux) || defined(__linux) || defined(__linux__)
 #  define CATCH_PLATFORM_LINUX
 
-#elif defined(__QNX__)
-#  define CATCH_PLATFORM_QNX
-
 #elif defined(WIN32) || defined(__WIN32__) || defined(_WIN32) || defined(_MSC_VER) || defined(__MINGW32__)
 #  define CATCH_PLATFORM_WINDOWS
 
@@ -9362,7 +9359,7 @@ namespace Catch {
         #define CATCH_TRAP()  __asm__(".inst 0xde01")
     #endif
 
-#elif defined(CATCH_PLATFORM_LINUX) || defined(CATCH_PLATFORM_QNX)
+#elif defined(CATCH_PLATFORM_LINUX)
     // If we can use inline assembler, do it because this allows us to break
     // directly at the location of the failing check instead of breaking inside
     // raise() called from it, i.e. one stack frame below.
