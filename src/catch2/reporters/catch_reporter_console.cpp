221d5ef2b431061626499b937fea1631539eecc2
Martin Hořeňovský <martin.horenovsky@gmail.com>
2025-07-16 14:34:21 +0200

    Toss locking from the console reporter

--- src/catch2/reporters/catch_reporter_console.cpp
+++ src/catch2/reporters/catch_reporter_console.cpp
@@ -19,7 +19,6 @@
 #include <catch2/internal/catch_console_width.hpp>
 #include <catch2/reporters/catch_reporter_helpers.hpp>
 #include <catch2/internal/catch_move_and_forward.hpp>
-#include <catch2/internal/catch_global_lock.hpp>
 #include <catch2/catch_get_random_seed.hpp>
 
 #include <cstdio>
@@ -463,10 +462,7 @@ void ConsoleReporter::sectionEnded(SectionStats const& _sectionStats) {
     StreamingReporterBase::sectionEnded(_sectionStats);
 }
 
-// Catch benchmark macros call these functions. Since catch internals are not thread-safe locking is needed.
-
 void ConsoleReporter::benchmarkPreparing( StringRef name ) {
-    auto lock = take_global_lock();
 	lazyPrintWithoutClosingBenchmarkTable();
 
 	auto nameCol = TextFlow::Column( static_cast<std::string>( name ) )
@@ -484,7 +480,6 @@ void ConsoleReporter::benchmarkPreparing( StringRef name ) {
 }
 
 void ConsoleReporter::benchmarkStarting(BenchmarkInfo const& info) {
-    auto lock = take_global_lock();
     (*m_tablePrinter) << info.samples << ColumnBreak()
         << info.iterations << ColumnBreak();
     if ( !m_config->benchmarkNoAnalysis() ) {
@@ -494,7 +489,6 @@ void ConsoleReporter::benchmarkStarting(BenchmarkInfo const& info) {
     ( *m_tablePrinter ) << OutputFlush{};
 }
 void ConsoleReporter::benchmarkEnded(BenchmarkStats<> const& stats) {
-    auto lock = take_global_lock();
     if (m_config->benchmarkNoAnalysis())
     {
         (*m_tablePrinter) << Duration(stats.mean.point.count()) << ColumnBreak();
@@ -512,7 +506,6 @@ void ConsoleReporter::benchmarkEnded(BenchmarkStats<> const& stats) {
 }
 
 void ConsoleReporter::benchmarkFailed( StringRef error ) {
-    auto lock = take_global_lock();
     auto guard = m_colour->guardColour( Colour::Red ).engage( m_stream );
     (*m_tablePrinter)
         << "Benchmark failed (" << error << ')'
