eb36f168908740baa677eed92d62c24f7248049f
Shloka Jain <sj@Shlokas-MacBook-Pro.local>
2025-08-10 16:52:49 +0530

    Fix color mode detection on FreeBSD by adding platform macro

--- src/catch2/internal/catch_console_colour.cpp
+++ src/catch2/internal/catch_console_colour.cpp
@@ -161,7 +161,7 @@ namespace {
 #endif // Windows/ ANSI/ None
 
 
-#if defined( CATCH_PLATFORM_LINUX ) || defined( CATCH_PLATFORM_MAC ) || defined( __GLIBC__ )
+#if defined( CATCH_PLATFORM_LINUX ) || defined( CATCH_PLATFORM_MAC ) || defined( __GLIBC__ ) || defined(__FreeBSD__)
 #    define CATCH_INTERNAL_HAS_ISATTY
 #    include <unistd.h>
 #endif
