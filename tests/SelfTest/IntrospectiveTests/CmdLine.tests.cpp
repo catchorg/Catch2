6ddb59179597374147017ec8960b880a83828c8b
qerased <amaz11amaz@gmail.com>
2025-12-20 21:19:03 +0300

    Add missing test for benchmark samples validation

--- tests/SelfTest/IntrospectiveTests/CmdLine.tests.cpp
+++ tests/SelfTest/IntrospectiveTests/CmdLine.tests.cpp
@@ -310,6 +310,15 @@ TEST_CASE( "Process can be configured on command line", "[config][command-line]"
                 ContainsSubstring("Benchmark samples must be greater than 0"));
         }
 
+        SECTION("samples must be parseable") {
+            auto result = cli.parse({"test", "--benchmark-samples=abc"});
+
+            CHECK_FALSE(result);
+            REQUIRE_THAT(
+                result.errorMessage(),
+                ContainsSubstring("Could not parse 'abc' as benchmark samples"));
+        }
+
         SECTION("resamples") {
             CHECK(cli.parse({ "test", "--benchmark-resamples=20000" }));
 
