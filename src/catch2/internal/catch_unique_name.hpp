b2d30b7bca92b2ff73652d83d1def4f37c7b9a93
Tanmoy Giri <tanmoygiri333@gmail.com>
2026-03-11 13:29:44 +0000

    Suppress __COUNTER__ pedantic warning on Clang
    Clang 22 with -pedantic-errors treats __COUNTER__ as a C2y
    extension and emits an error. Wrap the __COUNTER__ usage with
    diagnostic pragmas to suppress the warning on Clang.
    
    Fixes #3076

--- src/catch2/internal/catch_unique_name.hpp
+++ src/catch2/internal/catch_unique_name.hpp
@@ -11,8 +11,16 @@
 #include <catch2/internal/catch_config_counter.hpp>
 #define INTERNAL_CATCH_UNIQUE_NAME_LINE2( name, line ) name##line
 #define INTERNAL_CATCH_UNIQUE_NAME_LINE( name, line ) INTERNAL_CATCH_UNIQUE_NAME_LINE2( name, line )
+
 #ifdef CATCH_CONFIG_COUNTER
+#  ifdef __clang__
+#    pragma clang diagnostic push
+#    pragma clang diagnostic ignored "-Wc2y-extensions"
+#  endif
 #  define INTERNAL_CATCH_UNIQUE_NAME( name ) INTERNAL_CATCH_UNIQUE_NAME_LINE( name, __COUNTER__ )
+#  ifdef __clang__
+#    pragma clang diagnostic pop
+#  endif
 #else
 #  define INTERNAL_CATCH_UNIQUE_NAME( name ) INTERNAL_CATCH_UNIQUE_NAME_LINE( name, __LINE__ )
 #endif
